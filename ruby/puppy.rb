41d1800afe9a26f913a67294fb17e8d68d5225da
Ian Lucas <ianlucas1@MacBook-Pro-2.local>
2016-06-27 11:05:59 -0700

    Adds Release 0

---
+++ ruby/puppy.rb
@@ -0,0 +1,21 @@
+# Release 0: Design a Class
+
+# How might you design a Puppy class? What sorts of characteristics and behavior 
+# might a Puppy class involve? Using a format similar to the one we used for 
+# Eyelashus Adorableus, come up with some characteristics and behavior for a Puppy 
+# class. Use comments to add your class design to phase-0-tracks/ruby/puppy.rb.
+
+# SPECIES ------------------------
+# Puppy Class
+
+# CHARACTERISTICS ----------------
+# Ear length: varies
+# Leg Count: 4
+# Color: varies
+# Name: varies
+# Playful: yes
+
+# BEHAVIOR -----------------------
+# Jump
+# Lick
+# Scamper

dd7d9ae3aab97c39d23b64d4141476ed552fc6f3
Ian Lucas <ianlucas1@MacBook-Pro-2.local>
2016-06-28 09:51:39 -0700

    Adds some code; not done yet

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,27 @@
-class Puppy
+class Ninja
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
+  def initialize 
+    puts "Behold, a ninja is born ..."
+  end
+
+  def karate_chops(num_of_chops)
+    puts "The ninja chops #{num_of_chops} times."
+  end
+
+  def roundhouse_kicks(num_of_kicks) 
+      puts "The ninja roundhouse kicks #{num_of_kicks} times."
   end
 
 end
 
+ninja_ary = []
+
+3.times { |ninja|
+  ninja = Ninja.new
+  ninja_ary << ninja
+}
+
+ninja_ary.each { |ninja|  ninja.karate_chops(2) }
+ninja_ary.each { |ninja|  ninja.roundhouse_kicks(3) }
+
+p ninja_ary

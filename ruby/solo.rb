def53786d2e0251b10573bd9fb3f1be214fc9582
Ian Lucas <ianlucas1@MacBook-Pro-2.local>
2016-06-29 16:58:51 -0700

    Completes work requirements, but needs comments added

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,18 +1,14 @@
 # RELEASE 0: DESIGN A CLASS
 
-  # Program name:  Shock and Awe, A War Simulator 
-
-  # (side comment:  I haven't played a video game since Mario Kart in the mid-1990s 
-  #  and I'm not exactly a military expert)
+  # Program name:  Build a Military 
 
   # Requirement 1: three attributes (using at least two data types) 
   # whose values will vary from instance to instance (example: age)
 
     # Initialize a military organization with the following attributes
-    # A1: Branch (string like Air Force, Army, Navy, Marines, Coast Guard)
-    # A2: Number of personnel (random integer between 20 and 1 million)
-    # A3: Weapon system types (string like Planes, Ships, Guns)
-    # A4: Annual Budget (random boolean where true = a high budget and false = a low budget)
+    # A1: Branch (a string like Air Force, Army, Navy, Marines, Coast Guard)
+    # A2: Number of personnel (an integer)
+    # A3: Weapon system types (a string like Planes, Ships, Guns)
 
   # Requirement 2: three methods, at least one of which takes an argument
 
@@ -20,8 +16,6 @@
     # M2: Readiness (string like train, equip, stage supplies, spin-up)
     # M3: Combat Actions (string like deploy with a number of personnel, show-of-presence, 
     #     surgical-strike, shock-and-awe, battle-damage-assessment)
-
-  # Randomly choose branch, randomly choose conditional weapons systems, give a score, provide BDA
 
 #====================================#
 
@@ -31,114 +25,192 @@
 
 class Force
 
-  attr_reader :branch_type, :force_number
-  attr_accessor :num_of_personnel, :weapon_system
+  attr_reader :force_number, :branch_type
+  attr_accessor :num_of_personnel, :action_to_take, :op_name
 
-  def initialize(force_number, branch_type, num_of_personnel, weapon_system)
+  def initialize(force_number)
     @force_number = force_number
-    @branch_type = branch_type
-    @num_of_personnel = num_of_personnel
-    @weapon_system = weapon_system
-    #@annual_budget = annual_budget
+    @branch_type = nil
+    @num_of_personnel = nil
+    @action_to_take = nil
+    @op_name = nil
   end
 
-  def karate_chops(num_of_chops)
-    puts "The ninja chops #{num_of_chops} times."
+  def branch
+    loop do
+    puts "Which branch do you have an order for? Enter '1' for the Air Force, '2' for the Army, '3' for the Coast Guard, '4' for the Marines, or '5' for the Navy."  
+      branch_input = gets.to_i
+      if branch_input == 1
+        @branch_type = "Air Force" 
+        break
+      elsif branch_input == 2
+        @branch_type = "Army" 
+        break
+      elsif branch_input == 3
+        @branch_type = "Coast Guard" 
+        break
+      elsif branch_input == 4
+        @branch_type = "Marines" 
+        break
+      elsif branch_input == 5
+        @branch_type = "Navy" 
+        break
+      else
+        puts "I'm sorry, that's not a valid input."
+      end
+    end
   end
 
-  def roundhouse_kicks(num_of_kicks) 
-      puts "The ninja roundhouse kicks #{num_of_kicks} times."
+  def personnel
+    loop do
+
+      puts "How many #{branch_type.capitalize} personnel will this affect?"
+        num_of_personnel = gets.to_i
+
+      if num_of_personnel == 0
+        puts " "
+        puts "I'm sorry, please enter how many personnel one more time as a number like 1000."
+        puts " "
+      else  
+        @num_of_personnel = num_of_personnel.to_s.reverse.scan(/\d{1,3}/).join(",").reverse
+        break
+      end 
+
+    end
+  end
+
+  def action
+    loop do
+      puts "What would you like this force to do next? Enter '1' for equip, '2' for train, '3' for deploy, '4' for fight, or '5' for rest."
+        action_input = gets.to_i
+        
+        if action_input == 1
+          @action_to_take = "equip" 
+          break
+        elsif action_input == 2
+          @action_to_take = "train" 
+          break
+        elsif action_input == 3
+          @action_to_take = "deploy" 
+          break
+        elsif action_input == 4
+          @action_to_take = "fight" 
+          break
+        elsif action_input == 5
+          @action_to_take = "rest" 
+          break
+        else
+          puts "I'm sorry, that's not a valid input."
+        end
+    end
+  end
+
+  def change(change_input)
+    loop do
+    puts "Enter '1' to change the branch, '2' to change the number of personnel, '3' to change the action they take, or '4' to rename the operation."
+    what_to_change = gets.to_i
+
+      if what_to_change == 1
+          branch
+          break
+      elsif what_to_change == 2
+          personnel
+          break
+      elsif what_to_change == 3
+          action 
+          break
+      elsif what_to_change == 4
+          puts "Please give this operation a new name."
+            op_name = gets.chomp.upcase
+            @op_name = op_name 
+          break
+      else
+          puts "I'm sorry, that's not a valid input."
+      end      
+    end
+    puts " "
+    puts "I copy that, in OP #{op_name}, #{num_of_personnel} #{branch_type.capitalize} personnel will #{action_to_take}." 
   end
 
 end
 
-fighting_force_ary = []
-
 #====================================#
-
-# RELEASE 1:  USE YOUR CLASS IN A PROGRAM
 
 # USER INTERFACE
 
+force_ary = []
 force_count = 1
+
+puts "Good day, commander!"
 
 loop do
 
-  puts "What kind of branch do you want? (e.g. Air Force, Army, Navy, Marines, or Coast Guard)"
-    branch_type = gets.chomp.downcase
-
-    #if branch_type != "air force" || branch_type != army || branch_type != navy || branch_type != marines || branch_type != "coast guard"
-     # puts "I'm sorry, that's not a valid branch.  You'll have to try again."
-    #else
-     # puts "Okay, great."
-    #end
-
-
-  puts "Now we need to know if you want a large force or a small force.  Enter a number of personnel:"
-    num_of_personnel = gets.to_i
-
-
-  puts "What kind of weapon system would you like?  Enter 'planes', 'ships', or 'tanks'."
-    weapon_system = gets.chomp
-
-
-  fighting_force_ary << Force.new("#{force_count}", branch_type, num_of_personnel, weapon_system)
-
+  force = Force.new("#{force_count}")
   
-  puts "---------------------------------" 
-  puts "Summary of force ##{force_count}:"
-  puts "Branch - #{branch_type.capitalize}"
-  puts "Personnel - #{num_of_personnel}"
-  puts "Weapon System - #{weapon_system.capitalize}"
-  puts "---------------------------------" 
+  force_ary << force
+  
+  puts " "
 
+  force.branch
+  
+  puts " "
+  
+  force.personnel
+  
+  puts " "
+  
+  force.action
+  
+  puts " "
+  
+  puts "Please give this operation a name of your choosing. (e.g. 'Polishing Rubies' or 'Shock and Awe')"
+    op_name = gets.chomp.upcase
+
+  force.op_name = op_name
+
+  puts " "
+
+  puts "Roger that. During OP #{force.op_name}, #{force.num_of_personnel} #{force.branch_type.capitalize} personnel will #{force.action_to_take}."
+
+  loop do
+
+    puts " "
+
+    puts "Are there any changes to make to this order? Enter 'yes' or 'no'."
+      change_input = gets.chomp
+
+    if change_input == "no"
+      break
+    elsif change_input == "yes"
+      force.change(change_input)
+    else
+      puts "I'm sorry, that's not a valid input."
+    end
+
+  end
+
+  puts " "
 
   puts "Do you want to build another fighting force?  (valid inputs:  yes, no)"
     build_another_force = gets.chomp
 
   break if build_another_force == 'no'
 
-
   force_count += 1
 
 end
 
-  puts "---------------------------------"
+  puts " "
+
+  puts "Very good, sir. Here is a summary of today's order:"
   
-  fighting_force_ary.each { |force|  
-    puts "FORCE ##{force.force_number} STATUS:"
+  puts " "
+
+  force_ary.each { |force|  
+    puts "UNIT ##{force.force_number}:"
+    puts "Op Name - #{force.op_name}"
     puts "Branch - #{force.branch_type.capitalize}"
     puts "Personnel - #{force.num_of_personnel}"
-    puts "Weapon System - #{force.weapon_system.capitalize}"
-    puts "---------------------------------"   
+    puts "Current Order - #{force.action_to_take.capitalize}"
+    puts "-----------------------"   
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Arrays for initialization
-    Branch_ary = %w[ Air Force, Army, Navy, Marines, Coast Guard ]
-    Weapon_system_ary = %w[ planes ships guns ]
-
-#1.upto(3) { |force|
-#  fighting_force_ary << Force.new("#{force}", Branch_ary.sample, rand(20..1000000), Weapon_system_ary.sample, [true, false].sample)  
-#}
-
-
-  #santas[0].age = 2 #testing attr_accessor method
-  #santas[0].gender = "Male" #testing attr_accessor method
-  #p santas[0].ethnicity #testing getter/attr_reader method

ad89d7223dd8c28abd905dec9eaad3d93ce312f6
Ian Lucas <ianlucas1@MacBook-Pro-2.local>
2016-06-29 07:38:21 -0700

    initial commit of solo.rb on wrong branch

---
+++ ruby/solo.rb
@@ -0,0 +1,23 @@
+# Release 0: Design a Class
+
+# Program name:  Shock & Awe, A War Game (note:  I haven't played a video game since Mario Kart in the mid-1990s)
+
+# Requirement 1: three attributes (using at least two data types) 
+# whose values will vary from instance to instance (example: age)
+
+  # Initialize a military organization with the following attributes
+  # A1: Branch (string like Air Force, Army, Navy, Marines, Coast Guard)
+  # A2: Number of personnel (integer like 4,000)
+  # A3: Weapon system types (string like Planes, Ships, Guns)
+  # A4: Annual Budget (integer like 100,000,000, representing dollars)
+
+# Requirement 2: three methods, at least one of which takes an argument
+
+  # M1: Wellness (string like R&R, offering-transition-assistance, upgrading-housing)
+  # M2: Readiness (string like train, equip, stage supplies, spin-up)
+  # M3: Combat Actions (string like deploy with a number of personnel, show-of-presence, 
+  #     surgical-strike, shock-and-awe, battle-damage-assessment)
+
+
+
+

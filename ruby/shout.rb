de2531fd79eb512acb22a0d4f7e30ea5e3f32528
Ian Lucas <ianlucas1@MacBook-Pro-2.local>
2016-06-30 10:09:58 -0700

    Completes Release 1

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -2,9 +2,29 @@
 
 # R1.1 Declare your module. The syntax for declaring a 
 # module is pretty simple:
-  
-  # module Shout
-    # we'll put some methods here soon, but this code is fine for now!
+    
+  #module Shout
+
+  #  def self.yell_angrily(words) # R1.4
+  #    words + "!!!" + " :("
+  #  end
+
+  #  def self.yelling_happily(words) # R1.4
+  #    words + "!" + " :D"
+  #  end
+
   #end
 
-  
+# R1.5 Add driver code underneath your module declaration 
+# that calls both of your module methods. 
+
+# For some inexplicable reason, when I run the file in terminal
+# each of the following method calls print out twice
+# require_relative 'shout'
+  # p Shout.yell_angrily("Argh")
+  # p Shout.yelling_happily("Yay")
+
+#======================================#
+
+#Release 2: Mix in a Module
+

684060b53e27c24a16f2beea62376fb1907df10d
Ian Lucas <ianlucas1@MacBook-Pro-2.local>
2016-06-30 09:49:32 -0700

    Initial commit of shout.rb

---
+++ ruby/shout.rb
@@ -0,0 +1,10 @@
+# Release 1: Write a Simple Module
+
+# R1.1 Declare your module. The syntax for declaring a 
+# module is pretty simple:
+  
+  # module Shout
+    # we'll put some methods here soon, but this code is fine for now!
+  #end
+
+  

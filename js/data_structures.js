cb30ee6906b82e3e8a44c7c80e89d435af713eff
Ian Lucas <ianlucas1@MacBook-Pro-2.local>
2016-07-12 20:09:45 -0700

    Initial commit; completes release 0

---
+++ js/data_structures.js
@@ -0,0 +1,19 @@
+// declare two separate arrays:
+
+// an array of four colors ("blue", for instance, though you're welcome to 
+// get crazy with something like "iridescent blue/green with silver sparkles")
+
+// an array of four names that you might give a horse ("Ed" is always a solid 
+// choice to start with)
+
+var colors = ["Chestnut", "Black", "Buckskin", "Palomino"]
+var names = ["Sir Barton", "Gallant Fox", "War Admiral", "American Pharoah"]
+
+// Write additional code that adds another color to the colors array, and another horse name to the horses array
+
+colors.push("Bay")
+names.push("Seattle Slew")
+
+console.log(colors)
+console.log(names)
+

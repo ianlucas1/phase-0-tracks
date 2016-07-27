4b00d02301a29a55b06d130874b773fb957b25d3
Ian Lucas <ianlucas1@MacBook-Pro-2.local>
2016-07-26 18:21:50 -0700

    Completes C9.2

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,45 +1,45 @@
-console.log("The script is running!");
+var photos_ary = document.getElementsByClassName("image");
+wildfire = photos_ary[0];
+saved_kitten = photos_ary[1];
+saved_kitten.hidden = true;
 
-var photo = document.getElementById("wildfire");
-var photo = document.getElementById("wildfire");
+wildfire.addEventListener("mouseover", showMouseOver);
+wildfire.addEventListener("mouseout", showMouseOut);
+wildfire.addEventListener("click", switchPhotos);
+wildfire.addEventListener("click", showAlert);
 
-function initElement() {
-  photo.onmouseover = showMouseOver;
-  photo.onmouseout = showMouseOut;
-  photo.onclick = showAlert;
-};  
-
-function giveOrder() {
-  var paragraph = document.createElement("P");
+function giveOrder() { // the call for this function is in the button node of the index.html file
+  var paragraph = document.createElement("p");
   var text = document.createTextNode("Move your mouse over the fire to position your water balloon-equipped drone above it.");
   paragraph.appendChild(text);
-  document.body.appendChild(paragraph);
+  document.getElementById("button").appendChild(paragraph);
 };
 
-function showMouseOver() {  
+function showMouseOver(event) {  
   var notice = document.getElementById("notice");
-  notice.innerHTML = 'You are over the target now. Click it to release the water balloon!';
+  notice.innerHTML = "You're over the target! Click on the fire to release the water balloon!!";
 };
 
-function showMouseOut() {
+function showMouseOut(event) {
   var notice = document.getElementById("notice");
-  notice.innerHTML = 'You are no longer over the target.';
+  notice.innerHTML = "You're off the target. Hurry and put it out!";
 };
 
-function showAlert() {
-  alert("SPLASH! Congrats on a successful mission! You have save an untold number of wildcat kittens, for which the cat gods will surely reward you.")
+function showAlert(event) {
+  alert("SPLASH! You have save an untold number of kittens, for which the Cat Gods will surely reward you.")
 };
 
-function hidePhoto() {
-  event.target.hidden = true;
-};
+function switchPhotos(event) {
+  event.target.hidden = true;   
+  saved_kitten.hidden = false;
+  notice.hidden = true;
+}
 
-function addPinkBorder(event) {
-  event.target.style.border = "5px solid pink";
-};
 
-// photo.addEventListener("click", addPinkBorder);
-photo.addEventListener("click", hidePhoto);
+
+
+
+
 
 
 
